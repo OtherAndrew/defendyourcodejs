789b388f15a1322b7a1e988123956c66453e3364
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-06 17:55:34 -0700

    add number validation

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ import {input} from "@inquirer/prompts";
 const MAX_INT_32 = Math.pow(2, 31) - 1;
 const MIN_INT_32 = Math.pow(2, 31) * -1;
 
+let firstInt = 0;
+
 const validateName = (name) => {
     return /^[a-zA-Z]{1,50}$/.test(name)
         ? true
@@ -13,15 +15,30 @@ const validateName = (name) => {
 
 const clamp = (num, min, max) => Math.max(min, Math.min(num, max));
 
-const validateInt = (num) => {
-    return /^\d+$/.test(num) && num >= MIN_INT_32 && num <= MAX_INT_32
-        ? true
-        : "Please input a valid integer (max of 2^32 - 1, min of -2^32).";
+const validateInt = (num) => /^\d+$/.test(num) && num >= MIN_INT_32 && num <= MAX_INT_32;
+
+const validateFirstInt = (num) => {
+    const inputNum = parseInt(num);
+    if (validateInt(inputNum)) {
+        firstInt = inputNum;
+        return true;
+    } else {
+        return "Please input a valid integer (max of 2^31 - 1, min of -2^31)."
+    }
 }
 
-const validateIntAddition = (num1, num2) => validateInt(num1 + num2);
+const validateSecondInt = (num) => {
+    const inputNum = parseInt(num);
+    if (validateInt(inputNum)
+            && validateInt(firstInt + inputNum)
+            && validateInt(firstInt * inputNum)) {
+        return true
+    } else {
+        return "Please input a valid integer (max of 2^31 - 1, min of -2^31).";
+    }
+}
 
-const validateIntMultiplication = (num1, num2) => validateInt(num1 * num2);
+const formatNumber = (num) => new Intl.NumberFormat().format(num)
 
 inquirer
     .prompt([
@@ -41,21 +58,25 @@ inquirer
             type: input,
             name: 'firstInteger',
             message: 'Enter your first integer:',
-            validate: validateInt
+            validate: validateFirstInt
         },
         {
             type: input,
             name: 'secondInteger',
             message: 'Enter your second integer:',
-            validate: validateInt
+            validate: validateSecondInt
         }
     ])
     .then((answers) => {
+        const sum = formatNumber(
+            parseInt(answers['firstInteger']) + parseInt(answers['secondInteger']));
+        const product = formatNumber(
+            parseInt(answers['firstInteger']) * parseInt(answers['secondInteger']));
+        const firstIntOut = formatNumber(answers['firstInteger']);
+        const secondIntOut = formatNumber(answers['secondInteger']);
         console.log();
         console.log(`Hello, ${answers['firstName']} ${answers['lastName']}.`);
-        console.log(`Your two integers are ${answers['firstInteger']} and ${answers['secondInteger']}.`);
-        console.log(`The sum of ${answers['firstInteger']} and ${answers['secondInteger']}`
-            + ` is ${answers['firstInteger'] + answers['secondInteger']}.`);
-        console.log(`The product of ${answers['firstInteger']} and ${answers['secondInteger']}`
-            + ` is ${answers['firstInteger'] * answers['secondInteger']}.`);
+        console.log(`Your two integers are ${firstIntOut} and ${secondIntOut}.`);
+        console.log(`The sum of ${firstIntOut} and ${secondIntOut} is ${sum}.`);
+        console.log(`The product of ${firstIntOut} and ${secondIntOut} is ${product}.`);
     });
