ec99bb276af7dcb7d28384be5066ca416c47c539
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-07 00:17:24 -0700

    change fs operations to synchronous

--- main.js
+++ main.js
@@ -133,29 +133,54 @@ const writeToConsole = (answers) => {
     const product = formatNumber( parseInt(answers.firstInteger) * parseInt(answers.secondInteger));
     const firstIntOut = formatNumber(answers.firstInteger);
     const secondIntOut = formatNumber(answers.secondInteger);
+    const fileContents = fs.readFileSync(answers.fileName, 'utf8');
 
     console.log();
     console.log(`Hello, ${answers.firstName} ${answers.lastName}.`);
     console.log(`Your integers are ${firstIntOut} and ${secondIntOut}.`);
     console.log(`The sum of ${firstIntOut} and ${secondIntOut} is ${sum}.`);
     console.log(`The product of ${firstIntOut} and ${secondIntOut} is ${product}.`);
-    // https://nodejs.dev/en/learn/reading-files-with-nodejs/
-    fs.readFile(answers.fileName, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(`The text file you chose is "${answers.fileName}".`);
-        console.log(`The contents of "${answers.fileName}" are:`);
-        console.log(data);
-    });
-    // console.log(`Your password hash is: ${passwordHash}`);
+    console.log(`The text file you chose is "${answers.fileName}".`);
+    console.log(`The contents of "${answers.fileName}" are:`);
+    console.log(fileContents);
+}
+
+/**
+ * Writes user input to file.
+ * @param answers user input.
+ * @param answers.firstName the user's first name.
+ * @param answers.lastName the user's last name.
+ * @param answers.firstInteger the first integer the user provided.
+ * @param answers.secondInteger the last integer the user provided.
+ * @param answers.fileName the file name the user provided.
+ */
+const writeToFile = (answers) => {
+    const sum = formatNumber(parseInt(answers.firstInteger) + parseInt(answers.secondInteger));
+    const product = formatNumber( parseInt(answers.firstInteger) * parseInt(answers.secondInteger));
+    const firstIntOut = formatNumber(answers.firstInteger);
+    const secondIntOut = formatNumber(answers.secondInteger);
+    const outputFileName = `output/${answers.lastName}_${answers.firstName}_${Date.now()}.txt`;
+    const inputFileContents = fs.readFileSync(answers.fileName, 'utf8');
+    const outputFileContents = [
+        `First name: ${answers.firstName}`,
+        `Last name: ${answers.lastName}`,
+        `First integer: ${firstIntOut}`,
+        `Second integer: ${secondIntOut}`,
+        `Sum: ${sum}`,
+        `Product: ${product}`,
+        `Input file name: ${answers.fileName}`,
+        `Contents of "${answers.fileName}":`,
+        inputFileContents
+    ]
+    //https://nodejs.dev/en/learn/writing-files-with-nodejs/
+    fs.writeFileSync(outputFileName, outputFileContents.join('\n'));
+    console.log(`Saved results to: ${outputFileName}`);
 }
 
 // https://www.npmjs.com/package/inquirer
 inquirer
-    .prompt(questions)
+    .prompt(questions, () => {})
     .then((answers) => {
         writeToConsole(answers);
-        //writeToFile(answers);
+        writeToFile(answers);
     });
