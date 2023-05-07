e28d7516703268242b570f235f17092369aa75b1
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-07 00:30:41 -0700

    add jsdoc documentation

--- main.js
+++ main.js
@@ -11,6 +11,11 @@ const SALT_ROUNDS = 10;
 let firstInt;
 let passwordHash;
 
+/**
+ * Validates a given name. A name may only consist of up to 50 alphabetic characters.
+ * @param name The input name.
+ * @return {boolean|string} Error message if invalid, true if valid.
+ */
 const validateName = (name) => {
     if (!/^[a-zA-Z]{1,50}$/.test(name)) {
         return 'Please input a valid name (alphabetic characters, 50 characters max).';
@@ -18,8 +23,20 @@ const validateName = (name) => {
     return true;
 }
 
+/**
+ * Validates a given integer. An integer may only consist of numeric characters and is within the range of a 4 byte int
+ * (-2^31 <= num <= 2^31 - 1).
+ * @param num The input number.
+ * @return {boolean} if the input is an integer and within range.
+ */
 const validateInt = (num) => /^\d+$/.test(num) && num >= MIN_INT_32 && num <= MAX_INT_32;
 
+/**
+ * Validates a given integer. An integer may only consist of numeric characters and is within the range of a 4 byte int
+ * (-2^31 <= num <= 2^31 - 1).
+ * @param num The input number.
+ * @return {boolean|string} Error message if invalid, true if valid.
+ */
 const validateFirstInt = (num) => {
     const inputNum = parseInt(num);
     if (!validateInt(inputNum)) {
@@ -29,16 +46,33 @@ const validateFirstInt = (num) => {
     return true;
 }
 
+/**
+ * Validates a given integer. An integer may only consist of numeric characters and is within the range of a 4 byte int
+ * (-2^31 <= num <= 2^31 - 1). When added or multiplied with the first integer, it must not cause overflow or underflow.
+ * @param num The input number.
+ * @return {boolean|string} Error message if invalid, true if valid.
+ */
 const validateSecondInt = (num) => {
     const inputNum = parseInt(num);
     if (!validateInt(inputNum) || !validateInt(firstInt + num) || !validateInt(firstInt * num)) {
-        return 'Please input a valid integer (max of 2^31 - 1, min of -2^31).';
+        return 'Please input an integer that will not cause overflow or underflow when added or multiplied with' +
+            + ' the first integer inputted (max of 2^31 - 1, min of -2^31).';
     }
     return true;
 }
 
+/**
+ * Formats a number with an absolute value in the thousands with commas.
+ * @param num The number to format.
+ * @return {string} The number formatted with commas.
+ */
 const formatNumber = (num) => new Intl.NumberFormat().format(num)
 
+/**
+ * Validates a given file name. A file name is valid if it ends in ".txt" and it exists.
+ * @param filename The input file name.
+ * @return {boolean|string} Error message if invalid, true if valid.
+ */
 // https://stackoverflow.com/questions/71343219/i-want-to-check-if-this-file-in-this-directory-existing-or-not
 const validateTextFile = (filename) => {
     if (!/^.*\.txt$/.test(filename)) return 'Please input a valid file name (.txt files only).';
@@ -46,6 +80,16 @@ const validateTextFile = (filename) => {
     return true;
 }
 
+/**
+ * Validates a given password. A password is valid if it contains:
+ * - more than 8 characters
+ * - less than 50 characters
+ * - at least one capital letter
+ * - at least one number
+ * - at least one special character
+ * @param password The input password.
+ * @return {boolean|string} Error message if invalid, true if valid.
+ */
 const validatePassword = (password) => {
     if (password.length <= MIN_PASSWORD_LENGTH) {
         return `Password must be longer than ${MIN_PASSWORD_LENGTH} characters.`;
@@ -67,6 +111,11 @@ const validatePassword = (password) => {
     return true;
 }
 
+/**
+ * Confirms a given password. A password will be confirmed if the hash matches that of the first password.
+ * @param password The input confirm password.
+ * @return {boolean|string} Error message if invalid, true if valid.
+ */
 const validatePasswordConfirm = (password) => {
     if (!bcrypt.compareSync(password, passwordHash)) return 'Password does not match.';
     return true;
