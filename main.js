493a458ed13dfa53509908e2db20b3967b66a3d4
Andrew Nguyen <andrewnguyen841@gmail.com>
2023-05-06 17:15:00 -0700

    change prompts to format described in inquirer documentation

--- main.js
+++ main.js
@@ -1,49 +1,61 @@
-import { input } from '@inquirer/prompts';
+import inquirer from 'inquirer';
+import {input} from "@inquirer/prompts";
+
 
 const MAX_INT_32 = Math.pow(2, 31) - 1;
 const MIN_INT_32 = Math.pow(2, 31) * -1;
 
-const validateName = (name) => /^[a-zA-Z]{1,50}$/.test(name);
+const validateName = (name) => {
+    return /^[a-zA-Z]{1,50}$/.test(name)
+        ? true
+        : "Please input a valid name (alphabetic characters, 50 characters max).";
+}
 
 const clamp = (num, min, max) => Math.max(min, Math.min(num, max));
 
 const validateInt = (num) => {
-    return /^\d+$/.test(num)
-        && num >= MIN_INT_32
-        && num <= MAX_INT_32;
+    return /^\d+$/.test(num) && num >= MIN_INT_32 && num <= MAX_INT_32
+        ? true
+        : "Please input a valid integer (max of 2^32 - 1, min of -2^32).";
 }
 
 const validateIntAddition = (num1, num2) => validateInt(num1 + num2);
 
 const validateIntMultiplication = (num1, num2) => validateInt(num1 * num2);
 
-const firstName = await input({
-    message: 'Enter your first name:',
-    validate: validateName
-});
-
-const lastName = await input({
-    message: 'Enter your last name:',
-    validate: validateName
-});
-
-const firstInt = await input({
-    message: 'Enter your first integer:',
-    validate: validateInt
-});
-
-const secondInt = await input({
-    message: 'Enter your second integer:',
-    validate: validateInt
-});
-
-const filename = await input({
-    message: 'Enter an input file:'
-})
-
-console.log();
-console.log(`Hello, ${firstName} ${lastName}.`);
-console.log(`Your two integers are ${firstInt} and ${secondInt}.`);
-console.log(`The sum of ${firstInt} and ${secondInt} is ${firstInt + secondInt}.`);
-console.log(`The product of ${firstInt} and ${secondInt} is ${firstInt * secondInt}.`);
-console.log(`The file you chose is "${filename}".`);
+inquirer
+    .prompt([
+        {
+            type: input,
+            name: 'firstName',
+            message: 'Enter your first name:',
+            validate: validateName
+        },
+        {
+            type: input,
+            name: 'lastName',
+            message: 'Enter your last name:',
+            validate: validateName
+        },
+        {
+            type: input,
+            name: 'firstInteger',
+            message: 'Enter your first integer:',
+            validate: validateInt
+        },
+        {
+            type: input,
+            name: 'secondInteger',
+            message: 'Enter your second integer:',
+            validate: validateInt
+        }
+    ])
+    .then((answers) => {
+        console.log();
+        console.log(`Hello, ${answers['firstName']} ${answers['lastName']}.`);
+        console.log(`Your two integers are ${answers['firstInteger']} and ${answers['secondInteger']}.`);
+        console.log(`The sum of ${answers['firstInteger']} and ${answers['secondInteger']}`
+            + ` is ${answers['firstInteger'] + answers['secondInteger']}.`);
+        console.log(`The product of ${answers['firstInteger']} and ${answers['secondInteger']}`
+            + ` is ${answers['firstInteger'] * answers['secondInteger']}.`);
+    });
